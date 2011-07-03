de86379079bef607bba903c2127c5471bc762b8f
Thibaut Assus <thibaut.assus@gmail.com>
2011-07-03 07:18:22 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IndependantsFrancaisRb::Application.config.secret_token = '48d611c33467b1d7fe934e8cc7d34178ee0528b2aacc5e43bc6deb3443bca13ad9af3c387cf8f4acb43245932cbc475680ea44b839c73d38aed05f94ce1f5725'
